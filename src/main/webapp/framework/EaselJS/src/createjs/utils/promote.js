ffea15194f9cc250457a7e2a5803dd98ea005b4e
jiangyukun <187_6810_5877@sina.com>
2016-01-15 17:57:55 +0800

    code format

--- src/main/webapp/framework/EaselJS/src/createjs/utils/promote.js
+++ src/main/webapp/framework/EaselJS/src/createjs/utils/promote.js
@@ -1,37 +1,37 @@
 /*
-* promote
-* Visit http://createjs.com/ for documentation, updates and examples.
-*
-* Copyright (c) 2010 gskinner.com, inc.
-*
-* Permission is hereby granted, free of charge, to any person
-* obtaining a copy of this software and associated documentation
-* files (the "Software"), to deal in the Software without
-* restriction, including without limitation the rights to use,
-* copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the
-* Software is furnished to do so, subject to the following
-* conditions:
-*
-* The above copyright notice and this permission notice shall be
-* included in all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-* OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-* OTHER DEALINGS IN THE SOFTWARE.
-*/
+ * promote
+ * Visit http://createjs.com/ for documentation, updates and examples.
+ *
+ * Copyright (c) 2010 gskinner.com, inc.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 /**
  * @module CreateJS
  */
 
 // namespace:
-this.createjs = this.createjs||{};
+this.createjs = this.createjs || {};
 
 /**
  * @class Utility Methods
@@ -49,40 +49,42 @@ this.createjs = this.createjs||{};
  *
  * This should be called after the class's prototype is fully defined.
  *
- * 	function ClassA(name) {
+ *    function ClassA(name) {
  * 		this.name = name;
  * 	}
- * 	ClassA.prototype.greet = function() {
+ *    ClassA.prototype.greet = function() {
  * 		return "Hello "+this.name;
  * 	}
  *
- * 	function ClassB(name, punctuation) {
+ *    function ClassB(name, punctuation) {
  * 		this.ClassA_constructor(name);
  * 		this.punctuation = punctuation;
  * 	}
- * 	createjs.extend(ClassB, ClassA);
- * 	ClassB.prototype.greet = function() {
+ *    createjs.extend(ClassB, ClassA);
+ *    ClassB.prototype.greet = function() {
  * 		return this.ClassA_greet()+this.punctuation;
  * 	}
- * 	createjs.promote(ClassB, "ClassA");
+ *    createjs.promote(ClassB, "ClassA");
  *
- * 	var foo = new ClassB("World", "!?!");
- * 	console.log(foo.greet()); // Hello World!?!
+ *    var foo = new ClassB("World", "!?!");
+ *    console.log(foo.greet()); // Hello World!?!
  *
  * @method promote
  * @param {Function} subclass The class to promote super class methods on.
  * @param {String} prefix The prefix to add to the promoted method names. Usually the name of the superclass.
  * @return {Function} Returns the subclass.
  */
-createjs.promote = function(subclass, prefix) {
-	"use strict";
+createjs.promote = function (subclass, prefix) {
+    "use strict";
 
-	var subP = subclass.prototype, supP = (Object.getPrototypeOf&&Object.getPrototypeOf(subP))||subP.__proto__;
-	if (supP) {
-		subP[(prefix+="_") + "constructor"] = supP.constructor; // constructor is not always innumerable
-		for (var n in supP) {
-			if (subP.hasOwnProperty(n) && (typeof supP[n] == "function")) { subP[prefix + n] = supP[n]; }
-		}
-	}
-	return subclass;
+    var subP = subclass.prototype, supP = (Object.getPrototypeOf && Object.getPrototypeOf(subP)) || subP.__proto__;
+    if (supP) {
+        subP[(prefix += "_") + "constructor"] = supP.constructor; // constructor is not always innumerable
+        for (var n in supP) {
+            if (subP.hasOwnProperty(n) && (typeof supP[n] == "function")) {
+                subP[prefix + n] = supP[n];
+            }
+        }
+    }
+    return subclass;
 };
